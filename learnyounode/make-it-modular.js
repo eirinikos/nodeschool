420876ff484030bd68818a15690c364a138c7c42
Andrea <eirinikos@gmail.com>
2015-12-06 13:51:38 -0800

    make it modular exercise - in prog

--- learnyounode/make-it-modular.js
+++ learnyounode/make-it-modular.js
@@ -1,11 +1,11 @@
 // since this module exports only one function
 // mymodule itself can be called as a function
-var mymodule = require('./make-it-modular_module.js')
+var mymodule = require('./make-it-modular_module.js');
 
 var dir = process.argv[2];
 var ext = process.argv[3];
 
-console.log(mymodule)
+console.log(mymodule.foobar(dir, ext));
 
 // You must not print directly to the console from your module file,
 // only from your original program.  
