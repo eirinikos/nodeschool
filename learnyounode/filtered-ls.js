2ab7640cc3e2daac2387ac658b2cebcfa14e4390
Andrea <eirinikos@gmail.com>
2015-12-05 16:21:23 -0800

    review async-io, filtered-ls exercises

--- learnyounode/filtered-ls.js
+++ learnyounode/filtered-ls.js
@@ -1,22 +1,21 @@
 // print a list of files in given directory
 // filtered by the extension of the files
+// fs.readdir(path, callback) returns an array of the filenames in the directory
 
 // directory is process.argv[2]
 // extension to filter by is process.argv[3]
-
-// readdir reads the contents of directory
-// 'list' is array of filename strings
-// path.extname(p) returns file extension
+// process.argv[3] will not come prefixed with a '.'
 
 var fs = require('fs');
 var path = require('path');
 
-var p = process.argv[2];
-var extension = process.argv[3];
+var dir = process.argv[2];
+var ext = '.' + process.argv[3];
 
-fs.readdir(p, function callback (error, list) {
-  list.forEach(function (file) {
-    if (path.extname(file) === '.' + extension)
-      console.log(file);
-  });
-});
+fs.readdir(dir, function filtered(err, list){
+  if (err) return console.error(err)
+  list.forEach(function (filename) {
+    if (path.extname(filename) === ext)
+      console.log(filename);
+  })
+})
--- learnyounode/my-first-async-io.js
+++ learnyounode/my-first-async-io.js
@@ -1,6 +1,15 @@
 var fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function logMyNumber(error, fileContents) {
-  var lines = fileContents.split('\n').length - 1;
-  console.log(lines);
-})
+// fs.readFile(process.argv[2], 'utf8', function logMyNumber(error, fileContents) {
+//   var lines = fileContents.split('\n').length - 1;
+//   console.log(lines);
+// })
+
+// or...
+
+var contents = fs.readFile(process.argv[2], 'utf8', callback)
+
+function callback(err, contents){
+  if (err) { return console.error(err) };
+  console.log(contents.split('\n').length - 1);
+}
